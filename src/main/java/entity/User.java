4f33163117e3266ac4451b0e8b17d0e31a9aab73
skapc146 <skapcan52@gmail.com>
2024-11-08 11:26:01 -0500

    added starter code

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,4 +1,5 @@
-package main.java.entity;
+package entity;
+import java.util.ArrayList;
 import java.util.List;
 
 public class User
@@ -8,10 +9,43 @@ public class User
     String password;
     List<String> reviewHistory; // this might change
 
-    public User(String userID, String username, String password, List<String> reviewHistory){
-        this.userID = userID;
+    public User(String username, String password){
+        this.userID = generateUserID();
         this.username = username;
         this.password = password;
-        this.reviewHistory = reviewHistory;
+        this.reviewHistory = new ArrayList<String>();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    private String generateUserID(){
+        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                + "0123456789"
+                + "abcdefghijklmnopqrstuvxyz";
+
+        // create StringBuffer size of AlphaNumericString
+        final int USERID_LENGTH = 5;
+        StringBuilder sb = new StringBuilder(USERID_LENGTH);
+
+        for (int i = 0; i < USERID_LENGTH; i++) {
+
+            // generate a random number between
+            // 0 to AlphaNumericString variable length
+            int index
+                    = (int)(AlphaNumericString.length()
+                    * Math.random());
+
+            // add Character one by one in end of sb
+            sb.append(AlphaNumericString
+                    .charAt(index));
+        }
+
+        return sb.toString();
     }
 }
---
+++ src/main/java/use_case/logout/LogoutInteractor.java
@@ -0,0 +1,24 @@
+package use_case.logout;
+
+/**
+ * The Logout Interactor.
+ */
+public class LogoutInteractor implements LogoutInputBoundary {
+    private final LogoutUserDataAccessInterface userDataAccessObject;
+    private final LogoutOutputBoundary logoutPresenter;
+
+    public LogoutInteractor(LogoutUserDataAccessInterface userDataAccessInterface,
+                            LogoutOutputBoundary logoutOutputBoundary) {
+        this.userDataAccessObject = userDataAccessInterface;
+        this.logoutPresenter = logoutOutputBoundary;
+    }
+
+    @Override
+    public void execute(LogoutInputData logoutInputData) {
+        final String username = logoutInputData.getUsername();
+        userDataAccessObject.setCurrentUsername(null);
+        final LogoutOutputData outputData = new LogoutOutputData(username, false);
+        logoutPresenter.prepareSuccessView(outputData);
+    }
+}
+
