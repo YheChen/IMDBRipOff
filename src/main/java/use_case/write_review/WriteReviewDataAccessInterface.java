94bb9c5c5583a9a0c86511e19150e3e539f76cb2
skapc146 <skapcan52@gmail.com>
2024-11-14 12:36:15 -0500

    Created the data access interface for the write review use case.

---
+++ src/main/java/use_case/write_review/WriteReviewDataAccessInterface.java
@@ -0,0 +1,18 @@
+package use_case.write_review;
+import entity.Review;
+
+public interface WriteReviewDataAccessInterface {
+
+    /**
+     * Checks if the given review exists.
+     * @param id the id of the review to look for
+     * @return true if a review with the given id exists, false otherwise
+     */
+    boolean existsByID(String id);
+
+    /**
+     * Saves the review.
+     * @param review the review to save
+     */
+    void save(Review review);
+}
