02529d3f2f923e80d306c9bef294cec8b0512309
Rowan Houston <rowanhouston2@gmail.com>
2024-11-15 17:33:14 -0500

    Uncommented out a line that was previously breaking the code.

--- src/main/java/data_access/InMemoryMediaItemDataAccessObject.java
+++ src/main/java/data_access/InMemoryMediaItemDataAccessObject.java
@@ -7,5 +7,5 @@ import java.util.Map;
  * In-memory implementation of the DAO for storing media item data. This implementation does
  * NOT persist data between runs of the program.
  */
-//public class InMemoryMediaItemDataAccessObject implements SomethingInterface{
-//}
+public class InMemoryMediaItemDataAccessObject{
+}
