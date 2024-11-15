0093c581be0dd223fe18fc69e7eecedeaa0fc4ed
Rowan Houston <rowanhouston2@gmail.com>
2024-11-15 17:12:11 -0500

    Added initial code for usecase package files in BrowseReviews based on code from WriteReviews, and wrote initial code for BrowseReviews view.

---
+++ src/main/java/use_case/browse_reviews/BrowseReviewInputBoundary.java
@@ -0,0 +1,12 @@
+package use_case.browse_reviews;
+
+/**
+ * Input Boundary for actions which are related to logging in.
+ */
+public interface BrowseReviewInputBoundary {
+
+    /**
+     * Executes the write review use case.
+     */
+    void execute(BrowseReviewInputData writeReviewInputData);
+}
