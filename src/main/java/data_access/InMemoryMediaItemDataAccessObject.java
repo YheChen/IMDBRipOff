7ab210cc66e8f26b8b8e1c634378c5b461bc9f42
YheChen <yanzhenchen123@gmail.com>
2024-11-15 15:32:23 -0500

    created new dao

---
+++ src/main/java/data_access/InMemoryMediaItemDataAccessObject.java
@@ -0,0 +1,11 @@
+package data_access;
+import entity.MediaItem;
+
+import java.util.HashMap;
+import java.util.Map;
+/**
+ * In-memory implementation of the DAO for storing media item data. This implementation does
+ * NOT persist data between runs of the program.
+ */
+public class InMemoryMediaItemDataAccessObject{
+}
