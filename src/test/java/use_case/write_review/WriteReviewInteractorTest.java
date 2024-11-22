b777b7865cb171bb1819b7bfc0edb8bc2dd7d499
skapc146 <skapcan52@gmail.com>
2024-11-22 18:19:06 -0500

    updated the test case for the write review interactor

--- src/test/java/use_case/write_review/WriteReviewInteractorTest.java
+++ src/test/java/use_case/write_review/WriteReviewInteractorTest.java
@@ -7,8 +7,11 @@ import entity.User;
 import entity.UserFactory;
 import entity.Review;
 import org.junit.jupiter.api.Test;
+import use_case.login.LoginInputBoundary;
+import use_case.login.LoginInteractor;
 import use_case.login.LoginUserDataAccessInterface;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,19 +26,24 @@ class WriteReviewInteractorTest {
         WriteReviewInputData writeReviewInputData = new WriteReviewInputData("30ENX3",
                 "hello so yeah i really like this move idk why", 5,
                 "Harry Potter and the Philosopher's Stone");
+        InMemoryReviewDataAccessObject reviewMemObj = new InMemoryReviewDataAccessObject();
+        Review review = new Review("94xdj", "4o3me",
+                "hello so yeah i really like this move idk why", 5, LocalDate.now());
+        reviewMemObj.save(review);
 
         WriteReviewDataAccessInterface userRepository = new InMemoryReviewDataAccessObject();
 
         // This creates a successPresenter that tests whether the test case is as we expect.
         WriteReviewOutputBoundary successPresenter = new WriteReviewOutputBoundary() {
             @Override
-            public void prepareSuccessView(WriteReviewOutputData review) {
-                assertEquals(5, review.getRating()); // Check if the rating is correct
-                assertEquals("hello so yeah i really like this move idk why", review.getContent());
+            public void prepareSuccessView(WriteReviewOutputData writeReviewOutputData) {
+                assertEquals(5, reviewMemObj.getReview(review.getReviewID()).getRating()); // Check if the rating is correct
+                assertEquals("hello so yeah i really like this move idk why",
+                        reviewMemObj.getReview(review.getReviewID()).getContent());
                 // Check if the review content is correct
-                assertEquals("Harry Potter and the Philosopher's Stone" ,review.getMedia());
-                // Check if the movie/media is correct lol
-                assertEquals("30ENX3", review.getUserID());
+                assertEquals("Harry Potter and the Philosopher's Stone",
+                        reviewMemObj.getReview(review.getMediaID()).getContent());
+                // Check if the movie/media is correct lo
             }
         };
     }
